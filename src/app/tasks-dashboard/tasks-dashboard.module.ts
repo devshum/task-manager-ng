21127996a6f5fe98fa6432e0e3cf0df83a129c15
devshum <57689318+devshum@users.noreply.github.com>
2021-10-06 21:14:23 +0300

    es linter was set up | task interface was fixed

--- src/app/tasks-dashboard/tasks-dashboard.module.ts
+++ src/app/tasks-dashboard/tasks-dashboard.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { InlineSVGModule } from 'ng-inline-svg';
-import { NgModule } from "@angular/core";
-import { TaskComponent } from "./components/task/task.component";
+import { NgModule } from '@angular/core';
+import { TaskComponent } from './components/task/task.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
-import { TaskDashboardComponent } from "./containers/tasks-dashboard/tasks-dashboard.component";
+import { TaskDashboardComponent } from './containers/tasks-dashboard/tasks-dashboard.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksDashboardService } from '../core/services/tasks-dashboard.service';
 import {  ReactiveFormsModule } from '@angular/forms';
