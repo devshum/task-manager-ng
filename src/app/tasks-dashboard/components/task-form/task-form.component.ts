ea3d4d164a29ac23d08a9e8da542a172e3a5367a
devshum <57689318+devshum@users.noreply.github.com>
2021-11-23 22:16:03 +0200

    the status sort functionality was implemented

--- src/app/tasks-dashboard/components/task-form/task-form.component.ts
+++ src/app/tasks-dashboard/components/task-form/task-form.component.ts
@@ -1,21 +1,42 @@
 import { TaskView, TaskPostData } from './../../../core/models/task.interface';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { EnumStatus } from 'src/app/core/enums/task.statuses';
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.scss']
 })
 
-export class TaskFormComponent {
+export class TaskFormComponent implements OnInit {
   @Input() tasks: TaskView[] = [];
   @Output() hookTask: EventEmitter<TaskPostData> = new EventEmitter<TaskPostData>();
   @Output() formShown: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() selectStatus: EventEmitter<string> = new EventEmitter<string>();
   isFormShown = false;
+  selectedStatus: string;
+  statusOptions: string[] = [
+    EnumStatus.all,
+    EnumStatus.pending,
+    EnumStatus.closed,
+    EnumStatus.inProgress
+  ];
   constructor() {}
+
+  ngOnInit(): void {
+    this.selectedStatus = EnumStatus.all;
+  }
 
   toggleForm(): void {
     this.isFormShown = !this.isFormShown;
     this.formShown.emit(this.isFormShown);
+  }
+
+  selectStatusHandler(): void {
+    if(this.selectedStatus === EnumStatus.all) {
+      this.selectStatus.emit('');
+    } else {
+      this.selectStatus.emit(this.selectedStatus);
+    }
   }
 
   onHookTask(event: TaskPostData): void {
--- src/app/tasks-dashboard/containers/tasks-dashboard/tasks-dashboard.component.ts
+++ src/app/tasks-dashboard/containers/tasks-dashboard/tasks-dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { TasksDashboardService } from './../../../core/services/tasks-dashboard/tasks-dashboard.service';
 import { TaskView, TaskPostData } from '../../../core/models/task.interface';
-import { Observable, timer } from 'rxjs';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-tasks-dashboard',
   templateUrl: './tasks-dashboard.component.html',
@@ -13,6 +13,7 @@ import { Observable, timer } from 'rxjs';
 export class TaskDashboardComponent implements OnInit {
   tasks: TaskView[];
   isFormShown = false;
+  selectedStatus: string;
   loading$: Observable<boolean | null>;
 
   constructor(
@@ -21,47 +22,49 @@ export class TaskDashboardComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this._loaderService.start();
-    this._getTasksList();
+    this._getTasksList({sort: '-createdAt'});
   }
 
   onAddTask(event: TaskPostData): void {
-    this._loaderService.start();
     this._tasksService
       .addTask(event)
       .subscribe(() => {
-        this._getTasksList();
+        this._getTasksList({sort: '-createdAt'});
       });
   }
 
   onRemoveTask(event: TaskView): void {
-    this._loaderService.start();
     this._tasksService
       .removeTask(event)
       .subscribe(() => {
-        this._getTasksList();
+        this._getTasksList({sort: '-createdAt'});
       });
   }
 
   onEditTask(event: TaskView | any): void {
-    this._loaderService.start();
     this._tasksService
       .editTask(event)
       .subscribe(() => {
-        this._getTasksList();
+        this._getTasksList({sort: '-createdAt'});
       });
   }
 
-  onFormShown(event: boolean) {
+  onFormShown(event: boolean): void {
     this.isFormShown = event;
   }
 
-  private _getTasksList(): void {
+  onSelectStatus(event: string): void {
+    this.selectedStatus = event;
+    this._getTasksList({status: this.selectedStatus});
+  }
+
+  private _getTasksList(filterParams: any): void {
+    this._loaderService.start();
     this._tasksService
-      .getTasks()
+      .getTasks(filterParams)
       .subscribe(data => {
         this.tasks = data;
         this._loaderService.end();
-      });
+    });
   }
 }
