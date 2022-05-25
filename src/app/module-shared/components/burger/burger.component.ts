bbb4ee36a45681bf78cf081f814bd3e77207fc1e
devshum <57689318+devshum@users.noreply.github.com>
2022-05-25 15:29:21 +0300

    refactor burger

--- src/app/module-shared/components/burger/burger.component.ts
+++ src/app/module-shared/components/burger/burger.component.ts
@@ -1,19 +1,20 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { NavbarService } from './../../../core/services/navbar/navbar.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-burger',
   templateUrl: './burger.component.html',
   styleUrls: ['./burger.component.scss']
 })
-export class BurgerComponent {
-  @Output() isNavbarOpened: EventEmitter<boolean> = new EventEmitter<boolean>();
-  navbarOpened = false;
-  constructor() { }
+export class BurgerComponent implements OnInit {
+  public isNavbarOpened: boolean;
+  constructor(private _navbarService: NavbarService) { }
 
-  openNavbar() {
-    this.navbarOpened = !this.navbarOpened;
-
-    this.isNavbarOpened.emit(this.navbarOpened);
+  ngOnInit(): void {
+    this._navbarService.navbar$.subscribe(navbarStatus => this.isNavbarOpened = navbarStatus);
   }
 
+  toggleNavbar(): void {
+    this._navbarService.toggleNavbar();
+  }
 }
